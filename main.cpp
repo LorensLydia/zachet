8a2ab1a46496ac16f9907c50dfd480d3217eba97
LorensLydia <155399507+LorensLydia@users.noreply.github.com>
2024-06-11 10:00:38 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+class Divider {
+public:
+    Divider() {}
+
+    double divideNumbers(double num1, double num2) {
+        if (num2 == 0) {
+            std::cout << "Ошибка: деление на ноль невозможно." << std::endl;
+            return 0;
+        }
+        return num1 / num2;
+    }
+};
+
+int main() {
+    double number1, number2;
+
+    std::cout << "Введите первое число: ";
+    std::cin >> number1;
+
+    std::cout << "Введите второе число: ";
+    std::cin >> number2;
+
+    Divider divider;
+    double result = divider.divideNumbers(number1, number2);
+
+    std::cout << "Результат деления: " << result << std::endl;
+
+    return 0;
+}
