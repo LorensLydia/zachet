a7cc17af1f96adce309123d4c4b7997c10e2d062
LorensLydia <155399507+LorensLydia@users.noreply.github.com>
2024-06-13 10:10:58 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,9 +1,17 @@
-#include <iostream>
-#include "Divider.hpp"
+#include "gtest/gtest.h"
+#include "divider.hpp"
 
-   int main() {
-       Divider divider;
-       double result = divider.divideNumbers(10, 2);
-       std::cout << "Результат деления: " << result << std::endl;
-       return 0;
-   }
+TEST(DividerTest, DivideNumbers) {
+    Divider divider;
+    EXPECT_EQ(2.0, divider.divideNumbers(6.0, 3.0)); // Проверка деления двух чисел
+}
+
+TEST(DividerTest, DivideByZero) {
+    Divider divider;
+    EXPECT_THROW(divider.divideNumbers(5.0, 0.0), std::invalid_argument); // Проверка деления на ноль
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
