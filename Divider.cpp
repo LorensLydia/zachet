6e40b5df5a80611a241dc7d54426934bb885ae98
LorensLydia <155399507+LorensLydia@users.noreply.github.com>
2024-06-13 10:08:58 +0300

    Create Divider.cpp

---
+++ Divider.cpp
@@ -0,0 +1,12 @@
+#include "Divider.hpp"
+#include <iostream>
+
+Divider::Divider() {}
+
+double Divider::divideNumbers(double num1, double num2) {
+    if (num2 == 0) {
+        std::cout << "Ошибка: деление на ноль невозможно." << std::endl;
+        return 0;
+    }
+    return num1 / num2;
+}
