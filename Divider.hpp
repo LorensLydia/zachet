4457c2ebc99d44db8fef2485df62a25efe92e2e2
LorensLydia <155399507+LorensLydia@users.noreply.github.com>
2024-06-13 09:18:13 +0300

    Update and rename culc.hpp to Divider.hpp

---
+++ Divider.hpp
@@ -0,0 +1,14 @@
+Divider.hpp:
+#ifndef DIVIDER_HPP
+#define DIVIDER_HPP
+
+#include <iostream>
+
+class Divider {
+public:
+    Divider() {}
+
+    double divideNumbers(double num1, double num2);
+};
+
+#endif // DIVIDER_HPP
