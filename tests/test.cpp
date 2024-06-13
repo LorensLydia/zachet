02c29b2e8f5f78c5e04afc6d8f1bda0940206dfd
LorensLydia <155399507+LorensLydia@users.noreply.github.com>
2024-06-13 15:46:06 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,17 +1,56 @@
-#include "gtest/gtest.h"
-#include "divider.hpp"
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <iostream>
+#include <sstream>
 
-TEST(DividerTest, DivideNumbers) {
-    Divider divider;
-    EXPECT_EQ(2.0, divider.divideNumbers(6.0, 3.0)); // Проверка деления двух чисел
+class Prototype{
+public:
+	virtual ~Prototype(){}
+	virtual Prototype* Clone() = 0;
+protected:
+	Prototype(){}
+};
+class ConcretePrototype:public Prototype{
+public:
+	ConcretePrototype(){}
+	ConcretePrototype(const ConcretePrototype& cp){
+		*this = cp;
+		std::cout<<"ConcretePrototype copy ..."<<std::endl;
+	}
+	~ConcretePrototype(){}
+	virtual Prototype* Clone() {
+		return new ConcretePrototype(*this);
+	}
+};
+class PrototypeMock: public Prototype {
+public:
+    ~PrototypeMock() override = default;
+    MOCK_METHOD0(Clone, Prototype*());
+};
+
+TEST(Prototype_tests, test1) {
+    PrototypeMock p_mock;
+    EXPECT_CALL(p_mock, Clone()).Times(1);
+    Prototype* p1 = p_mock.Clone();
 }
 
-TEST(DividerTest, DivideByZero) {
-    Divider divider;
-    EXPECT_THROW(divider.divideNumbers(5.0, 0.0), std::invalid_argument); // Проверка деления на ноль
+TEST(Prototype_tests, test2) {
+    PrototypeMock p_mock;
+    EXPECT_CALL(p_mock, Clone()).Times(1);
+    Prototype* p1 = p_mock.Clone();
+    PrototypeMock p1_mock;
+    EXPECT_CALL(p1_mock, Clone()).Times(1);
+    Prototype* p2 = p1_mock.Clone();
 }
 
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+TEST(Prototype_tests, test3) {
+    std::ostringstream oss;
+    std::streambuf* cout_streambuf = std::cout.rdbuf();
+    std::cout.rdbuf(oss.rdbuf());
+
+    Prototype* p = new ConcretePrototype();
+    Prototype* p1 = p->Clone();
+
+    std::cout.rdbuf(cout_streambuf);
+    EXPECT_EQ(oss.str(), "ConcretePrototype copy ...\n");
 }
